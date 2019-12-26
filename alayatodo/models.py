34d7a181d03ee41ca9c39a2d44c63d30ed4e584d
Guilherme Souza <gsouza@newstore.com>
2019-12-26 18:41:13 +0100

    fix(models): Fixing Todo description and loading

--- alayatodo/models.py
+++ alayatodo/models.py
@@ -4,7 +4,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    todos = db.relationship('Todo', back_populates="user")
+    todos = db.relationship('Todo', back_populates="user", lazy=True)
 
     def __repr__(self):
         return '<User {self.username}>'
@@ -15,7 +15,7 @@ class Todo(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
         nullable=False)
     user = db.relationship("User", back_populates="todos")
-    description = db.Column(db.String(255), unique=True, nullable=False)
+    description = db.Column(db.String(255), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def as_dict(self):
